d1f1a4b2e849182eed3e70586883a0b5aff944b0
Allen Pittard <apittard@synalloy.com>
2019-10-02 12:14:43 -0400

    Remove unused curl call

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,6 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('/api/weather/latitude/{latitude}/longitude/{longitude}', function ($latitude, $longitude) {
-    $url  = 'http://api.weatherbit.io/v2.0/current';
-    $curl = curl_init();
-
-    curl_setopt_array($curl, array(
-        CURLOPT_URL            => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT        => 30,
-        CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST  => "GET",
-        CURLOPT_HTTPHEADER     => [
-            "cache-control: no-cache"
-        ],
-    ));
-
-    $response = curl_exec($curl);
-    $err      = curl_error($curl);
-
-    curl_close($curl);
-
     $sampleApiResponse = '{  
         "data":[  
            {  
