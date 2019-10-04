b0570f09e37a5b4cda2405ab11ab0f512ab85b35
Allen Pittard <apittard@synalloy.com>
2019-10-04 09:08:24 -0400

    Change slider icon to fa calendar

--- frontend/components/SliderIcon/SliderIcon.jsx
+++ frontend/components/SliderIcon/SliderIcon.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { FaChevronCircleLeft } from 'react-icons/fa';
+import { FaCalendar } from 'react-icons/fa';
 import './SliderIcon.css';
 
 const SliderIcon = props => {
     return (
         <span className="slider-icon" onClick={props.onClick}>
-            <FaChevronCircleLeft />
+            <FaCalendar />
         </span>
     );
 };
